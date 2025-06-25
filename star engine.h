76291282cb6756874cc62c3d8ef89e9d619d5a9f
xsq12348 <112300877+xsq12348@users.noreply.github.com>
2025-06-25 20:40:13 +0800

    Update star engine.h

--- star engine.h
+++ star engine.h
@@ -31,10 +31,15 @@
 1.02 修复了部分BUG
 1.1 添加了简单物理
 1.2 cpp移植成功,现在cpp和都可以同时使用该引擎
+1.3 添加了OpenCL支持
 */
 #pragma once
 #undef STARDLC
 #include"star.h"
+#if STARTOpenCL
+#include<CL/opencl.h>
+#pragma comment(lib,"OpenCL.lib")
+#endif
 
 #define VK_0 48
 #define VK_1 49
@@ -138,6 +143,30 @@ typedef struct
 	BOOL button;
 	BOOL triggered;
 }BUTTON;
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+}POINT3D;
+typedef struct
+{
+	float x;
+	float y;
+	float Z;
+}POINT2D;
+typedef struct
+{
+	POINT3D camerapoint;
+	POINT3D cameravectorx;
+	POINT3D cameravectory;
+	float cameravectorxmo;
+	float cameravectorymo;
+	float focalLength;
+	float matrixR[3][3];
+	float matrix[3][3];
+}CAMERA;
 
 //--------------------------------------------------------------------------------------绘图函数----------------------------------------------------------------------------------------------------------//
 
@@ -309,6 +338,223 @@ void InitialisationCircle(CIRCLEPHYSICS* circle, int x, int y, double m, double 
 	circle->mass = m;
 }
 
+//-----------------------------------------------------------------------------------------3D-------------------------------------------------------------------------------------------------------------//
+void CameraMatrix(CAMERA* camera, double Rx, double Ry, double Rz)
+{
+
+	/*
+	The results of the math software calculations
+	>> Announcement : Welcome to math procedure
+	>> Read "matrix.txt"
+	>> Var matrixR  = "matrix.txt"->A
+	>> Var matrixRy = "matrix.txt"->B
+	>> Var matrixRz = "matrix.txt"->C
+	>> Var matrixRx = "matrix.txt"->D
+	>> Var matrixRc
+	>> JudgmentLogic matrixR
+	>> No Logic
+	>> Var rx="matrix.txt"->rx
+	>> Var ry="matrix.txt"->ry
+	>> Var rz="matrix.txt"->rz
+	>> matrixRc = matrixRy * matrixRx
+	>> Print Var matrixRc Standard
+	>> matrixRc :
+	{
+		 cos(ry), sin(ry) * sin(rx), sin(ry) * cos(rx);
+			   0,			cos(rx),		  -sin(rx);
+		-sin(ry), cos(ry) * sin(rx), cos(ry) * cos(rx);
+	}
+	>> matrixR = matrixRc * matrixRz
+	>> Print Var matrixR Standard
+	>> matrixR :
+	{
+		 cos(ry) * cos(rz)+ sin(ry) * sin(rx) * sin(rz),cos(ry) * -sin(rz)+ sin(ry) * sin(rx) * cos(rz),sin(ry) * cos(rx);
+									  cos(rx) * sin(rz),							  cos(rx) * cos(rz),		 -sin(rx);
+		-sin(ry) * cos(rz)+ cos(ry) * sin(rx) * sin(rz),sin(ry) * sin(rz) + cos(ry) * sin(rx) * cos(rz),cos(ry) * cos(rx);
+	}
+	>> Write matrixR to "matrix.txt"->A
+	>> Exit
+	>> Announcement : Byebye
+
+x and y are the spatial orientation vectors of the camera
+Z vectors can be calculated by fork multiplication
+	From the x and y directions of the camera, you can define a two-dimensional tensor space that opens up in space, and according to the following matrix transformation,
+you can get an orthogonal projection, and then you can get the perspective effect by perspective division, although it is more troublesome than a homogeneous matrix, but it is easier to understand and write code
+
+		matrixA =
+		|x.x, y.x|
+		|x.y, y.y|
+		|x.z, y.z|
+
+		matrixB =
+		|x.x, x.y, x.z|
+		|y.x, y.y, y.z|
+
+		matrixC =(matrixB * matrixA)^-1
+
+		(matrixB * matrixA)^-1 =
+		|y.x * y.x + y.y * y.y + y.z * y.z, -(x.x * y.x + x.y * y.y + x.z * y.z)|
+		|-(x.x * y.x + x.y * y.y + x.z * y.z), x.x * x.x + x.y * x.y + x.z * x.z|
+		matrix = matrixA * (matrixB * matrixA)^-1 * matrixB
+
+		p = matrix * a
+
+	double matrixA[2][3] =
+	{
+		{ x.x, x.y, x.z},
+		{ y.x, y.y, y.z}
+	};
+
+	double matrixB[3][2] =
+	{
+		{ x.x, y.x},
+		{ x.y, y.y},
+		{ x.z, y.z}
+	};
+	*/
+
+	double
+		rx = DegRad(Rx),
+		ry = DegRad(Ry),
+		rz = DegRad(Rz);
+	double matrixR[3][3];
+
+	matrixR[0][0] = cos(ry) * cos(rz) + sin(ry) * sin(rx) * sin(rz);
+	matrixR[0][1] = -cos(ry) * sin(rz) + sin(ry) * sin(rx) * cos(rz);
+	matrixR[0][2] = sin(ry) * cos(rx);
+	matrixR[1][0] = cos(rx) * sin(rz);
+	matrixR[1][1] = cos(rx) * cos(rz);
+	matrixR[1][2] = -sin(rx);
+	matrixR[2][0] = -sin(ry) * cos(rz) + cos(ry) * sin(rx) * sin(rz);
+	matrixR[2][1] = sin(ry) * sin(rz) + cos(ry) * sin(rx) * cos(rz);
+	matrixR[2][2] = cos(ry) * cos(rx);
+
+	double
+		buffera = camera->cameravectorx.x * camera->cameravectorx.x + camera->cameravectorx.y * camera->cameravectorx.y + camera->cameravectorx.z * camera->cameravectorx.z,
+		bufferb = camera->cameravectory.x * camera->cameravectory.x + camera->cameravectory.y * camera->cameravectory.y + camera->cameravectory.z * camera->cameravectory.z,
+		bufferc = -(camera->cameravectorx.x * camera->cameravectory.x + camera->cameravectorx.y * camera->cameravectory.y + camera->cameravectorx.z * camera->cameravectory.z);
+	double matrix[3][3];
+	matrix[0][0] = (camera->cameravectorx.x * bufferb + camera->cameravectory.x * bufferc) * camera->cameravectorx.x + (camera->cameravectorx.x * bufferc + camera->cameravectory.x * buffera) * camera->cameravectory.x;
+	matrix[1][0] = (camera->cameravectorx.y * bufferb + camera->cameravectory.y * bufferc) * camera->cameravectorx.x + (camera->cameravectorx.y * bufferc + camera->cameravectory.y * buffera) * camera->cameravectory.x;
+	matrix[2][0] = (camera->cameravectorx.z * bufferb + camera->cameravectory.z * bufferc) * camera->cameravectorx.x + (camera->cameravectorx.z * bufferc + camera->cameravectory.z * buffera) * camera->cameravectory.x;
+	matrix[0][1] = (camera->cameravectorx.x * bufferb + camera->cameravectory.x * bufferc) * camera->cameravectorx.y + (camera->cameravectorx.x * bufferc + camera->cameravectory.x * buffera) * camera->cameravectory.y;
+	matrix[1][1] = (camera->cameravectorx.y * bufferb + camera->cameravectory.y * bufferc) * camera->cameravectorx.y + (camera->cameravectorx.y * bufferc + camera->cameravectory.y * buffera) * camera->cameravectory.y;
+	matrix[2][1] = (camera->cameravectorx.z * bufferb + camera->cameravectory.z * bufferc) * camera->cameravectorx.y + (camera->cameravectorx.z * bufferc + camera->cameravectory.z * buffera) * camera->cameravectory.y;
+	matrix[0][2] = (camera->cameravectorx.x * bufferb + camera->cameravectory.x * bufferc) * camera->cameravectorx.z + (camera->cameravectorx.x * bufferc + camera->cameravectory.x * buffera) * camera->cameravectory.z;
+	matrix[1][2] = (camera->cameravectorx.y * bufferb + camera->cameravectory.y * bufferc) * camera->cameravectorx.z + (camera->cameravectorx.y * bufferc + camera->cameravectory.y * buffera) * camera->cameravectory.z;
+	matrix[2][2] = (camera->cameravectorx.z * bufferb + camera->cameravectory.z * bufferc) * camera->cameravectorx.z + (camera->cameravectorx.z * bufferc + camera->cameravectory.z * buffera) * camera->cameravectory.z;
+	for (int i = 0; i < 3; i++)
+		for (int o = 0; o < 3; o++)
+		{
+			camera->matrixR[o][i] = matrixR[o][i];
+			camera->matrix[o][i] = matrix[o][i];
+		}
+
+}
+
+void CameraCompute(CAMERA* camera, double Rx, double Ry, double Rz)
+{
+	CameraMatrix(camera, Rx, Ry, Rz);
+	camera->cameravectorx.x = camera->matrixR[0][0] * camera->cameravectorxmo;
+	camera->cameravectorx.y = camera->matrixR[1][0] * camera->cameravectorxmo;
+	camera->cameravectorx.z = camera->matrixR[2][0] * camera->cameravectorxmo;
+
+	camera->cameravectory.x = camera->matrixR[0][1] * camera->cameravectorymo;
+	camera->cameravectory.y = camera->matrixR[1][1] * camera->cameravectorymo;
+	camera->cameravectory.z = camera->matrixR[2][1] * camera->cameravectorymo;
+}
+
+POINT2D Point3DDrawing(POINT3D point, CAMERA camera, double L, BOOL orthogonal)
+{
+
+	POINT3D cameravectory =
+	{
+			camera.cameravectorx.y * camera.cameravectory.z - camera.cameravectorx.z * camera.cameravectory.y,
+			camera.cameravectorx.z * camera.cameravectory.x - camera.cameravectorx.x * camera.cameravectory.z,
+			camera.cameravectorx.x * camera.cameravectory.y - camera.cameravectorx.y * camera.cameravectory.x
+	};
+	POINT3D relativecoordinatespointbuffer =
+	{
+		-point.x + camera.camerapoint.x,
+		-point.y + camera.camerapoint.y,
+		-point.z + camera.camerapoint.z,
+	};
+	double direction = (relativecoordinatespointbuffer.x * cameravectory.x + relativecoordinatespointbuffer.y * cameravectory.y + relativecoordinatespointbuffer.z * cameravectory.z) / (sqrt(cameravectory.x * cameravectory.x + cameravectory.y * cameravectory.y + cameravectory.z * cameravectory.z) * sqrt(relativecoordinatespointbuffer.x * relativecoordinatespointbuffer.x + relativecoordinatespointbuffer.y * relativecoordinatespointbuffer.y + relativecoordinatespointbuffer.z * relativecoordinatespointbuffer.z));
+	POINT2D EOR = { Error,Error,Error };
+	if (direction >= -L)return EOR;
+
+	POINT3D  relativecoordinatespoint = relativecoordinatespointbuffer;
+	relativecoordinatespointbuffer.x = relativecoordinatespoint.x * camera.matrix[0][0] + relativecoordinatespoint.y * camera.matrix[1][0] + relativecoordinatespoint.z * camera.matrix[2][0];
+	relativecoordinatespointbuffer.y = relativecoordinatespoint.x * camera.matrix[0][1] + relativecoordinatespoint.y * camera.matrix[1][1] + relativecoordinatespoint.z * camera.matrix[2][1];
+	relativecoordinatespointbuffer.z = relativecoordinatespoint.x * camera.matrix[0][2] + relativecoordinatespoint.y * camera.matrix[1][2] + relativecoordinatespoint.z * camera.matrix[2][2];
+
+	POINT2D returnpoint =
+	{
+		 relativecoordinatespointbuffer.x * camera.cameravectorx.x + relativecoordinatespointbuffer.y * camera.cameravectorx.y + relativecoordinatespointbuffer.z * camera.cameravectorx.z ,
+		 relativecoordinatespointbuffer.x * camera.cameravectory.x + relativecoordinatespointbuffer.y * camera.cameravectory.y + relativecoordinatespointbuffer.z * camera.cameravectory.z ,
+		 0
+	};
+
+	if (!orthogonal)
+	{
+		double pointmo[2] =
+		{
+			sqrt(relativecoordinatespointbuffer.x * relativecoordinatespointbuffer.x + relativecoordinatespointbuffer.y * relativecoordinatespointbuffer.y + relativecoordinatespointbuffer.z * relativecoordinatespointbuffer.z),
+			sqrt(relativecoordinatespoint.x * relativecoordinatespoint.x + relativecoordinatespoint.y * relativecoordinatespoint.y + relativecoordinatespoint.z * relativecoordinatespoint.z)
+		};
+
+		returnpoint.Z = sqrt(pointmo[1] * pointmo[1] - pointmo[0] * pointmo[0]);
+		returnpoint.x = returnpoint.x / returnpoint.Z * camera.focalLength;
+		returnpoint.y = returnpoint.y / returnpoint.Z * camera.focalLength;
+		if (returnpoint.Z > 5000)return EOR;
+	}
+	else returnpoint.Z = 1;
+	return returnpoint;
+}
+#if STARTOpenCL
+
+cl_platform_id platformid;
+cl_device_id deviceid;
+cl_context context;
+cl_command_queue commandqueue;
+cl_program program;
+cl_kernel kernel;
+
+char* STAROpenCL3D =
+"__kernel void Point3DDrawing(__global float* pointx, __global float* pointy, __global float* pointz, __global float* cameravectorx, __global float* cameravectory, __global float* camerapoint, __global float* cameramatrix, __global float* returnpointx, __global float* returnpointy, __global float* returnpointz,__global float* focalLength)"
+"{"
+"	int number=get_global_id(0);"
+"	float cameravectorZ[3];"
+"	cameravectorZ[0] = cameravectorx[1] * cameravectory[2] - cameravectorx[2] * cameravectory[1];"
+"	cameravectorZ[1] = cameravectorx[2] * cameravectory[0] - cameravectorx[0] * cameravectory[2];"
+"	cameravectorZ[2] = cameravectorx[0] * cameravectory[1] - cameravectorx[1] * cameravectory[0];"
+"	float relativecoordinatespointbuffer[3];"
+"	relativecoordinatespointbuffer[0] = - pointx[number] + camerapoint[0];"
+"	relativecoordinatespointbuffer[1] = - pointy[number] + camerapoint[1];"
+"	relativecoordinatespointbuffer[2] = - pointz[number] + camerapoint[2];"
+"	float direction = (relativecoordinatespointbuffer[0] * cameravectorZ[0] + relativecoordinatespointbuffer[1] * cameravectorZ[1] + relativecoordinatespointbuffer[2] * cameravectorZ[2]) / (sqrt(cameravectorZ[0] * cameravectorZ[0] + cameravectorZ[1] * cameravectorZ[1] + cameravectorZ[2] * cameravectorZ[2]) * sqrt(relativecoordinatespointbuffer[0] * relativecoordinatespointbuffer[0] + relativecoordinatespointbuffer[1] * relativecoordinatespointbuffer[1] + relativecoordinatespointbuffer[2] * relativecoordinatespointbuffer[2]));"
+"	if(direction >= -.1f){returnpointz[number] = -1; return;}"
+"	float relativecoordinatespoint[3];"
+"	relativecoordinatespoint[0]= relativecoordinatespointbuffer[0];"
+"	relativecoordinatespoint[1]= relativecoordinatespointbuffer[1];"
+"	relativecoordinatespoint[2]= relativecoordinatespointbuffer[2];"
+"	relativecoordinatespointbuffer[0] = relativecoordinatespoint[0] * cameramatrix[0] + relativecoordinatespoint[1] * cameramatrix[1] + relativecoordinatespoint[2] * cameramatrix[2];"
+"	relativecoordinatespointbuffer[1] = relativecoordinatespoint[0] * cameramatrix[3] + relativecoordinatespoint[1] * cameramatrix[4] + relativecoordinatespoint[2] * cameramatrix[5];"
+"	relativecoordinatespointbuffer[2] = relativecoordinatespoint[0] * cameramatrix[6] + relativecoordinatespoint[1] * cameramatrix[7] + relativecoordinatespoint[2] * cameramatrix[8];"
+"	returnpointx[number] = relativecoordinatespointbuffer[0] * cameravectorx[0] + relativecoordinatespointbuffer[1] * cameravectorx[1] + relativecoordinatespointbuffer[2] * cameravectorx[2];"
+"	returnpointy[number] = relativecoordinatespointbuffer[0] * cameravectory[0] + relativecoordinatespointbuffer[1] * cameravectory[1] + relativecoordinatespointbuffer[2] * cameravectory[2];"
+"	returnpointz[number] = 0;"
+"	float pointmo[2];"
+"	pointmo[0] = sqrt(relativecoordinatespointbuffer[0] * relativecoordinatespointbuffer[0] + relativecoordinatespointbuffer[1] * relativecoordinatespointbuffer[1] + relativecoordinatespointbuffer[2] * relativecoordinatespointbuffer[2]);"
+"	pointmo[1] = sqrt(relativecoordinatespoint[0] * relativecoordinatespoint[0] + relativecoordinatespoint[1] * relativecoordinatespoint[1] + relativecoordinatespoint[2] * relativecoordinatespoint[2]);"
+"	returnpointz[number] = sqrt(pointmo[1] * pointmo[1] - pointmo[0] * pointmo[0]);"
+"	returnpointx[number] = returnpointx[number] / returnpointz[number] * *focalLength;"
+"	returnpointy[number] = returnpointy[number] / returnpointz[number] * *focalLength;"
+"	if (returnpointz[number] > 5000){returnpointz[number] = -1;return;}"
+"	return;"
+"}"
+;
+#endif
 //--------------------------------------------------------------------------------------游戏流程----------------------------------------------------------------------------------------------------------//
 
 //初始化游戏
@@ -374,6 +620,16 @@ THREAD GameThreadLogic(LPARAM lparam)
 //游戏循环
 void GameLoop(GAME* Game, BOOL esc)
 {
+#if STARTOpenCL
+	cl_int error;
+	clGetPlatformIDs(1, &platformid, NULL);
+	clGetDeviceIDs(platformid, CL_DEVICE_TYPE_GPU, 1, &deviceid, NULL);
+	context = clCreateContext(NULL, 1, &deviceid, NULL, NULL, NULL);
+	commandqueue = clCreateCommandQueue(context, deviceid, NULL, NULL);
+	program = clCreateProgramWithSource(context, 1, (const char**)&STAROpenCL3D, NULL, &error);
+	clBuildProgram(program, 1, &deviceid, NULL, NULL, NULL);
+	kernel = clCreateKernel(program, "Point3DDrawing", NULL);
+#endif
 	GAMEDEAD = 0;
 	GAMETHEARDLOGIC = Game;
 	Game->escswitch = esc;
@@ -406,6 +662,10 @@ void GameOver(GAME* Game, BOOL cmdswitch)
 	DeletBuffer(Game->doublebuffer.hBitmap, Game->doublebuffer.hdc);	//销毁双缓冲资源
 	DeletWindow(Game->Windowhwnd);										//删除游戏窗口
 	if (cmdswitch)CMD(ON);												//是否在游戏结束时恢复控制台窗口
-	//PostQuitMessage(0);
+#if STARTOpenCL
+	clReleaseProgram(program);
+	clReleaseDevice(deviceid);
+	clReleaseContext(context);
+#endif
 	printf("游戏资源清理完成!");
 }
